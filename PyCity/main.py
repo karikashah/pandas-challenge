fe041ad5b9ce38b07b75b0a14a2f8677eb2cc80b
Karika Shah <karikashah@yahoo.com>
2018-12-12 13:22:35 -0600

    Accomplished District summary dataframe

--- PyCity/main.py
+++ PyCity/main.py
@@ -9,36 +9,47 @@ students_csv = "../../../Python Resources/students_complete.csv"
 schools_df = pdk.read_csv(schools_csv)
 students_df = pdk.read_csv(students_csv)
 
-# print its first 5 rows & also count of row n column
-# print(schools_df.head())
-# print(schools_df.shape)
-# print("------------------------------------------------------------------")
-# print(students_df.head())
-# print(students_df.shape)
-# print("------------------------------------------------------------------")
-
 # Combine the data into a single dataset
 school_data_complete = pdk.merge(schools_df, students_df, how="left", on=["school_name", "school_name"])
 
-# print(school_data_complete.head())
-# print(school_data_complete.shape)
-# print("------------------------------------------------------------------")
-
+##################### CALCULATIONS FOR DISTRICT SUMMARY DATAFRAME ------------------------------------------------------
 # calculate total # of schools
 num_of_schools = school_data_complete["school_name"].nunique()
-print("------------------------------------------------------------------")
-print(f"Total # of Schools are {num_of_schools}")
 
 # calculate the total number of students
-num_of_students = school_data_complete["student_name"].nunique()
-print(f"Total # of Students are {num_of_students}")
+num_of_students = school_data_complete["student_name"].count()
 
 # calculate the total budget
 total_budget = schools_df["budget"].sum()
-print(f"Total budget for school is ${total_budget}") # !!!need to take care of formating!!!
 
-# calculate the average math & reading scores 
+# calculate the average math, reading scores & overall passing rate
 avg_math = school_data_complete["math_score"].mean()
 avg_reading = school_data_complete["reading_score"].mean()
+overall_pass_rate = (avg_math+avg_reading)/2
 print(f"Average Math score is {avg_math}")
 print(f"Average Reading score is {avg_reading}")
+
+# calculate the percentage of students with a passing math & reading score (70 or greater)
+num_passing_math = students_df.loc[students_df['math_score'] >= 70]['math_score'].count()
+perc_pass_math = (num_passing_math/num_of_students)*100
+
+num_passing_reading = students_df.loc[students_df['reading_score'] >= 70]['reading_score'].count()
+perc_pass_reading = (num_passing_reading/num_of_students)*100
+
+district_summary = pdk.DataFrame({
+        "Total Schools" : [num_of_schools],
+        "Total Students": [num_of_students],
+        "Total Budget": [total_budget],
+        "Average Math Score": [avg_math],
+        "Average Reading Score": [avg_reading],
+        "% Passing Math": [perc_pass_math],
+        "% Passing Reading": [perc_pass_reading],
+        "Overall Passing Rate": [overall_pass_rate]
+    }
+)
+
+# formating & styling the district summary data frame
+district_summary["Total Students"] = district_summary["Total Students"].map("{:,}".format)
+district_summary["Total Budget"] = district_summary["Total Budget"].map("${:,.2f}".format)
+
+print(district_summary.head())
